29bfd6ae32af1df70925208e7d144c66bd410ecc
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-05-09 16:11:32 -0600

    Add items controller class

---
+++ warehouse/src/main/java/shopify/warehouse/controllers/ItemsController.java
@@ -0,0 +1,49 @@
+package shopify.warehouse.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import shopify.warehouse.models.Item;
+import shopify.warehouse.repositories.ItemRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/inventory")
+public class ItemsController {
+
+    private final ItemRepository itemRepository;
+
+    public ItemsController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping
+    public List<Item> getItems() {
+        return itemRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Item getItem(@PathVariable Long id) throws NoSuchElementException {
+        return itemRepository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public Item createItem(@RequestBody Item item) {
+        return itemRepository.save(item);
+    }
+
+    @PutMapping("/{id}")
+    public Item updateItem(@PathVariable Long id, @RequestBody Item item) throws NoSuchElementException {
+        Item currentItem = itemRepository.findById(id).orElseThrow();
+        currentItem.setName(item.getName());
+        currentItem.setAmount(item.getAmount());
+        currentItem.setDescription(item.getDescription());
+        return itemRepository.save(item);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteItem(@PathVariable Long id) {
+        itemRepository.deleteById(id);
+        return !itemRepository.findById(id).isPresent();
+    }
+}
