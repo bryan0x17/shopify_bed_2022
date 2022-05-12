0a65d084d58ece23ef51284f8a9047fc83d0ab53
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-05-12 12:27:53 -0600

    Add edit item functionality

--- warehouse/src/main/resources/static/inventory.js
+++ warehouse/src/main/resources/static/inventory.js
@@ -2,7 +2,7 @@ async function getInventory() {
   const response = await fetch("/inventory");
   const data = await response.json();
   renderInventoryTable(data);
-};
+}
 
 function renderInventoryTable(data) {
   const tableElement = document.getElementById("table");
@@ -13,7 +13,7 @@ function renderInventoryTable(data) {
   } else {
     data.forEach((item) => {
       tableElement.innerHTML = `
-        <tr>
+        <tr id="${item.id}">
             <td>${item.name}</td>
             <td>${item.description}</td>
             <td>${item.amount}</td>
@@ -24,6 +24,33 @@ function renderInventoryTable(data) {
         </tr>`;
     });
   }
+}
+
+async function editItem(id) {
+  const row = document.getElementById(id);
+  const item = await getInventoryItem(id);
+  console.log(item);
+  if (item) {
+    row.innerHTML = `
+        <td>
+            <input type="text" name="name" id="name" value="${item.name}"></td>
+        <td>
+            <input type="text" name="description" id="description" value="${item.description}"></td>
+        <td>
+            <input type="number" name="amount" id="amount" value="${item.amount}"></td>
+        <td>
+            <button onclick="saveItem(${item.id}">Save</button>
+        </td>
+        `;
+  } else {
+      window.alert("Item can't be edited");
+  }
+  
+}
+
+async function getInventoryItem(id) {
+    return fetch('/inventory/' + id)
+        .then(response => response.json());
 }
 
 getInventory();
